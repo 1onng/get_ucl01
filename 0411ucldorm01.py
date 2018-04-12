4e7dfbbc7c1a1a4bf159bab0b64138d20b3cea2e
1onng <38302605+1onng@users.noreply.github.com>
2018-04-12 08:36:18 +0800

    Add files via upload

---
+++ 0411ucldorm01.py
@@ -0,0 +1,46 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+mainData=[]
+
+url='http://www.ucl.ac.uk/prospective-students/accommodation/residences/houses'
+response=requests.get(url)
+soup=BeautifulSoup(response.text,'lxml')
+arts=soup.find('tbody')
+articles=arts.find_all('tr')
+
+for article in articles:
+    name=article.find('td','align-left all-vp name').getText().strip()
+    distance=article.find('td','align-left extra').getText().strip().replace("\r\n",'').replace(" ",'')
+    meta=article.find('td','align-left all-vp name').find('a')
+    links=meta.get('href')
+
+
+    res=requests.get(links)
+    soup=BeautifulSoup(res.text,'lxml')
+    arts2=soup.find('tbody')
+    articles2=arts2.find_all('tr','even')
+    for article2 in articles2:
+        roomtype=article2.find('span','roomtype').getText().strip()
+        other=article2.find_all('td')
+        rates=other[1].text.replace('£','')
+        rate=float(rates)
+        numbers=other[2].text
+        number=float(numbers)
+        print(name,distance,roomtype,rate,number)
+        itemall={'name':name,'distance':distance,'roomtype':roomtype,'rate':rate,'number':number}
+        mainData.append(itemall)
+
+
+
+
+with open('get_ucldorm03.csv','w') as csvfile:
+    fieldnames=['name','distance','roomtype','rate','number']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for item in mainData:
+        print (item)
+        writer.writerow(item)
+
+print("Writing csv complete")
